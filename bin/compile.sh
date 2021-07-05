97b3046658000ddf1a48e54033968845db7152ab
Henrik Giesel <hengiesel@gmail.com>
2021-07-05 14:28:38 +0200

    Clear dist before building

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,5 +1,7 @@
 declare DIR="$(cd "$(dirname "$0")/.." && pwd -P)"
 
+rm -rf "$DIR/dist/*"
+
 # for filename in "$DIR/designer/"*'.ui'; do
 #   pyuic5 "$filename" > "$DIR/gui/forms/$(basename ${filename%.*})_ui.py"
 # done
