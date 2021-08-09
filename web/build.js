81e012a0d7eb19e1fbdcf4454f1991055db3d96d
Henrik Giesel <hengiesel@gmail.com>
2021-08-10 01:07:22 +0200

    Use svelte cross unit

--- web/build.js
+++ web/build.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const esbuild = require("esbuild");
 const sveltePlugin = require("esbuild-svelte");
+const crossUnit = require("svelte-cross-unit");
 
 if (!fs.existsSync("../dist")) {
   fs.mkdirSync("../dist");
@@ -8,43 +9,6 @@ if (!fs.existsSync("../dist")) {
 
 if (!fs.existsSync("../dist/web")) {
   fs.mkdirSync("../dist/web");
-}
-
-const svelteGlobalPattern = /^const (.*?) = (.*?);$/gmsu;
-const globalIdentifier = 'globalThis.svelteCrossUnit';
-
-function compileAsServer(input) {
-    const code = input.replaceAll(
-        svelteGlobalPattern,
-        (_match, identifier, value) => `const ${identifier} = ${globalIdentifier}.${identifier} = ${value}`,
-    )
-
-    return `${globalIdentifier} = {};\n${code}`;
-}
-
-function compileAsClient(input) {
-    return input.replaceAll(
-        svelteGlobalPattern,
-        (_match, identifier) => `const ${identifier} = ${globalIdentifier}.${identifier}`,
-    );
-}
-
-function esbuildPlugin({ server = false } = {}) {
-    return {
-        name: "svelte-cross-unit",
-        setup: (build) => {
-            build.onLoad({ filter: /.*/ }, async (args) => {
-                if (!args.path.endsWith("index.mjs")) {
-                    return;
-                }
-
-                const source = await fs.promises.readFile(args.path, "utf8");
-                const contents = server ? compileAsServer(source) : compileAsClient(source);
-
-                return { contents };
-            });
-        },
-    };
 }
 
 esbuild
@@ -55,7 +19,10 @@ esbuild
     minify: false /* do not set this to true */,
     bundle: true,
     splitting: false,
-    plugins: [sveltePlugin(), esbuildPlugin()],
+    plugins: [
+        sveltePlugin(),
+        crossUnit.esbuildPlugin()
+    ],
   })
   .catch((err) => {
     console.error(err);
