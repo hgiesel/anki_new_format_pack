a77276db90f53016f28278cec72e8f97a0dc57bc
Henrik Giesel <hengiesel@gmail.com>
2021-08-09 15:54:21 +0200

    Avoid zipping ./pyc files

--- bin/compile.sh
+++ bin/compile.sh
@@ -2,14 +2,12 @@ declare DIR="$(cd "$(dirname "$0")/.." && pwd -P)"
 
 rm -rf "$DIR/dist/*"
 
-# for filename in "$DIR/designer/"*'.ui'; do
-#   pyuic5 "$filename" > "$DIR/gui/forms/$(basename ${filename%.*})_ui.py"
-# done
-
 cp -rf "$DIR/__init__.py" \
-  "$DIR/src" \
   "$DIR/manifest.json" \
   "$DIR/dist"
+
+mkdir -p "$DIR/dist/src"
+find . -wholename "./src/*.py" -exec cp --parents '{}' "$DIR/dist" \;
 
 yarn --cwd "$DIR/web" build
 
