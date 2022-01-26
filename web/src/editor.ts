e658772cde700e0794a8329d13b8b9bbf0163c05
Henrik Giesel <hengiesel@gmail.com>
2022-01-26 19:36:01 +0100

    Remove unnecessary arguments to prettier

--- web/src/editor.ts
+++ web/src/editor.ts
@@ -3,9 +3,9 @@ import Paragraph from "./Paragraph.svelte";
 import HorizontalRuler from "./HorizontalRuler.svelte";
 import CodeBlock from "./CodeBlock.svelte";
 
-import * as NoteEditor from "anki/NoteEditor.svelte";
+import * as NoteEditor from "anki/NoteEditor";
 
-NoteEditor.lifecycle.onMount(({ toolbar }: NoteEditorAPI): void => {
+NoteEditor.lifecycle.onMount(({ toolbar }): void => {
     toolbar.formatInlineButtons.append({ component: StrikeThrough }, 2);
     toolbar.formatBlockButtons.append({ component: Paragraph });
     toolbar.formatBlockButtons.append({ component: HorizontalRuler });
