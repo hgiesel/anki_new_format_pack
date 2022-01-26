af494c6f13320f248c245642e784b50b8a11bb56
Henrik Giesel <hengiesel@gmail.com>
2022-01-27 00:39:17 +0100

    Remove live-server
    - Will not be needed, because we're mostly working through Anki's servers

--- web/esbuild.config.js
+++ web/esbuild.config.js
@@ -3,7 +3,6 @@ const { build } = require("esbuild");
 const sveltePreprocess = require("svelte-preprocess");
 const sveltePlugin = require("esbuild-svelte");
 const sassPlugin = require("esbuild-sass-plugin").default;
-const liveServer = require("live-server");
 
 for (const dir of ["../dist", "../dist/web"]) {
     if (!fs.existsSync(dir)) {
@@ -12,30 +11,34 @@ for (const dir of ["../dist", "../dist/web"]) {
 }
 
 const production = process.env.NODE_ENV === "production";
+const development = process.env.NODE_ENV === "development";
 
-// esbuild watch in dev mode to rebuild out files
-let watch = false;
-if (process.argv[2] === "watch") {
-    watch = {
+const watch = development
+    ? {
         onRebuild(error) {
-            if (error)
+            if (error) {
                 console.error("esbuild: Watch build failed:", error.getMessage());
-            else console.log("esbuild: Watch build succeeded");
-        },
-    };
-    liveServer.start({
-        port: 8082, // Set the server port. Defaults to 8082.
-        root: "./public", // Set root directory that's being served. Defaults to cwd.
-        open: false, // When false, it won't load your browser by default.
-        wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
-        logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
-    });
-}
+            } else {
+                console.log("esbuild: Watch build succeeded");
+            }
+        }
+    }
+    : false;
 
-// Esbuild build options
-// See: https://esbuild.github.io/api/#build-api
+/**
+ * This should point to all entry points for scripts.
+ * Each one will create one js and one css file in `../src/dist/web'
+ */
+const entryPoints = [
+    "src/editor.ts",
+];
+
+/**
+ * Esbuild build options
+ * See https://esbuild.github.io/api/#build-api for more
+ */
 const options = {
-    entryPoints: ["./src/editor.ts"],
+    entryPoints,
     outdir: "../dist/web",
     format: "iife",
     target: "es6",
