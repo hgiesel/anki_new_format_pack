f8de2811dc513c81707dacf251be962a99ed37b9
Henrik Giesel <hengiesel@gmail.com>
2022-01-27 21:35:53 +0100

    Add some documentation regardings imports

--- ts/src/editor.ts
+++ ts/src/editor.ts
@@ -5,9 +5,36 @@ import Paragraph from "./Paragraph.svelte";
 import HorizontalRuler from "./HorizontalRuler.svelte";
 import CodeBlock from "./CodeBlock.svelte";
 
+/**
+ * This is how you can import from Anki:
+ */
 import * as NoteEditor from "anki/NoteEditor";
 
-NoteEditor.lifecycle.onMount(({ toolbar }): void => {
+/**
+ * The import above looks like a normal static ESM import, however it is not.
+ * This is the magic of esbuild. During compilation, the expression above will be translated into:
+
+ * ```ts
+ * const NoteEditor = require("anki/NoteEditor");
+ * ```
+ *
+ * This means that you also dynamically import packages exported from Anki:
+ * TODO
+ */
+
+/**
+ * You can use imports prefixed with `@anki' to import types directly
+ * from the Anki source. This can be helpful for typing callbacks (see below).
+ *
+ * Disclaimers:
+ * 1. Do no try to import anything but types in this manner! It will not work.
+ * 2. Types have less guarantee to stay unchanged a/o be moved to other files.
+ *    This will not break you build however, as esbuild does depend on Typescript
+ *    types for a build to succeed.
+ */
+import type { NoteEditorAPI } from "@anki/editor/NoteEditor.svelte";
+
+NoteEditor.lifecycle.onMount(({ toolbar }: NoteEditorAPI): void => {
     toolbar.formatInlineButtons.append({ component: StrikeThrough }, 2);
     toolbar.formatBlockButtons.append({ component: Paragraph });
     toolbar.formatBlockButtons.append({ component: HorizontalRuler });
